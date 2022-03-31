924e6a1a28709edaef60c7e9247a7b16647ce2bb
Maritza Esparza <maritza.esparza@wizeline.com>
2022-03-30 18:24:50 -0600

    :bug: Workaround for the gcsfs dependency

--- local/start_airflow.sh
+++ local/start_airflow.sh
@@ -6,6 +6,11 @@ export AIRFLOW__CORE__LOAD_EXAMPLES=False
 # Initiliase the metadatabase
 airflow db init
 
+# FIXME: Installing gcsfs here because when installing in Dockerfile
+# there's a dependency issue so this is a workaround
+pip install gcsfs==2022.2.0
+
+
 # Create User
 airflow users create -e "admin@airflow.com" -f "airflow" -l "airflow" -p "airflow" -r "Admin" -u "airflow"
 
